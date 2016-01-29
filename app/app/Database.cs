f0f143b6626474dd7962998ae12c9d6997fb933e
jordivanderhek <jordivanderhek@gmail.com>
2016-01-29 17:03:40 +0100

    added example Project, with database class

---
+++ app/app/Database.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace app
+{
+    class Database
+    {
+        public static string connectieString = @"Data Source=DATABASEADRESS;Initial Catalog=DATABASENAME;Integrated Security=True";
+
+        public static DataTable sqlQuery(String QueryString)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectieString))
+                {
+                    SqlCommand cmd = conn.CreateCommand();
+                    cmd.CommandText = QueryString;
+
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable("Table");
+                    da.Fill(dt);
+                    return dt;
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+
+        }
+    }
+}
